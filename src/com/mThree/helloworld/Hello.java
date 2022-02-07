cbb7d8c73ed6cdfece8077bcc6ea82d11ee09fa5
Brendan Ewing <EwingBrendan32@gmail.com>
2022-02-07 17:15:22 -0500

    HelloWorld

---
+++ src/com/mThree/helloworld/Hello.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @author Brendan Ewing
+ * email: EwingBrendan32@gmail.com
+ * Date: 2/7/22
+ *  purpose: My first for this training hello world program in an ide (eclipse)
+ */
+package com.mThree.helloworld;
+public class Hello {
+	public static void main (String[] args) {
+		String address= "263 s 10th st, newark, NJ";
+		String phone = "201-468-3982";
+		String a = "Who lives at: \t";
+		String b = "And phone number is: \t";
+				
+		System.out.println("Hello, World!");
+		System.out.println("Brendan Ewing");
+		System.out.println(a+address);
+		System.out.println(b+phone);
+	}
+
+}
